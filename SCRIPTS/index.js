52ac16d4282104a402ff035e20adcd470ebef2d8
ian-pablo <167139972+ian-pablo@users.noreply.github.com>
2024-10-08 22:13:54 -0300

    Teste de certeza
    upload novamente de todos os arquivos para que haja certeza que tudo estará ok

---
+++ SCRIPTS/index.js
@@ -0,0 +1,10 @@
+{
+    const foguete = document.querySelector("#rocket");
+    const terra = document.querySelector("#earth");
+
+    function animação(){
+        
+    }
+
+    requestAnimationFrame(animação);
+}
