6572d7764451408fbd8703a985524a7f093d37fc
ian-pablo <167139972+ian-pablo@users.noreply.github.com>
2024-11-16 20:08:56 -0300

    Knowledge Update
    Apenas que... Busquem conhecimento

--- SCRIPTS/botaoAbertura.js
+++ SCRIPTS/botaoAbertura.js
@@ -2,7 +2,7 @@ function abrir(conteudoCartao, cartao) {
 
     // consegue os elementos em si
     let _conteudoCartao = document.querySelector(`${conteudoCartao}`);
-    let _cartao = document.querySelector(`${cartao}`)
+    let _cartao = document.querySelector(`${cartao}`);
 
     // consegue a propriedade --time do 'cartao'
     let tempoAnimacao = parseFloat( getComputedStyle(_cartao).getPropertyValue('--time') );
@@ -10,35 +10,29 @@ function abrir(conteudoCartao, cartao) {
     //estoca o 'display' do '_cartao'
     let _display = _cartao.style.display;
 
-    // caso o display seja nulo muda ele para 'none'
-    if (_display == ""){
-        _display = "none";
-    }
-
-    _cartao.transition = "height var(--time) ease-in;";
-    _conteudoCartao.transition = "transform var(--time) ease-in;";
 
     // altera a sua propriedade display
-    if (_display == "none") {
+    if (_display == "none" || _display == "") {
 
         _cartao.style.display = "block";
-
+        // console.log(getComputedStyle(_cartao).getPropertyValue('height'));
+        // console.log(getComputedStyle(document.querySelector('html')).getPropertyValue('height'));
+        
         setTimeout(() => {
-            _cartao.style.height = 'var(--height)';
+            _cartao.style.height = "max-content";
             _conteudoCartao.style.transform = "rotateX(0deg)";
         }, 0);
 
 
     } else {
-
+        // console.log(getComputedStyle(_cartao).getPropertyValue('height'));
+        // console.log(getComputedStyle(document.querySelector('html')).getPropertyValue('height'));
         _conteudoCartao.style.transform = "rotateX(90deg)";
-        _cartao.style.height = "0px";
 
         setTimeout(() => {
+            _cartao.style.height = '0px';
             _cartao.style.display = "none";
-            
-        }, tempoAnimacao * 1000);
-        
+        }, tempoAnimacao * 1200);
 
     }
 
